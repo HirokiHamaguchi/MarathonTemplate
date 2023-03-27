16c93b62ea11836559ff78e1581bf5e85bc5a100
hari64 <hari64boli64@gmail.com>
2023-03-27 09:28:00 +0900

    fix path

--- src/bash/init.sh
+++ src/bash/init.sh
@@ -4,7 +4,7 @@ if [ -f src/cpp/viewer.hpp.gch ]; then
     echo "viewer.hpp.gch already exists"
 else
     echo "now precompiling viewer.hpp..."
-    /usr/bin/g++-10 -x c++-header -g -std=c++17 -Dhari64 -Wall -Wextra -fdiagnostics-color=always -D_GLIBCXX_DEBUG viewer.hpp
+    /usr/bin/g++-10 -x c++-header -g -std=c++17 -Dhari64 -Wall -Wextra -fdiagnostics-color=always -D_GLIBCXX_DEBUG src/cpp/viewer.hpp
 fi
 
 export DISPLAY=:0.0
